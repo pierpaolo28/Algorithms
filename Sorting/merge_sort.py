ba9c7a19aa94b35baa802f03ec0c269218e217f8
ppi1u16 <ppi1u16@ecs.soton.ac.uk>
2019-06-09 09:30:40 +0100

    merge_sort

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,1 +1,38 @@
-print("Hello")
+import math
+
+
+def merge_sort(inp):
+    if len(inp) > 1:
+        mid = math.floor(len(inp)/2)
+        left = inp[:mid]
+        right = inp[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] >= right[j]:
+                inp[k] = right[j]
+                j += 1
+            else:
+                inp[k] = left[i]
+                i += 1
+            k += 1
+
+        while j < len(right):
+            inp[k] = right[j]
+            j += 1
+            k += 1
+
+        while i < len(left):
+            inp[k] = left[i]
+            i += 1
+            k += 1
+    return inp
+
+
+arr = [1, 5, 2, 87, 33, 21, 78]
+print(arr)
+print(merge_sort(arr))
